e46193879e7bf4b3b01cacb9b6f4716599091251
Maksym Tereshyn <max.tereshyn@gmail.com>
2022-12-16 17:58:45 +0200

    functional almost done

---
+++ src/Fields/InputField.vue
@@ -0,0 +1,18 @@
+<template>
+  <input type="text" />
+</template>
+
+<script>
+const EVENTS = {
+  input: "input",
+};
+
+export default {
+  name: "input-field",
+  data() {
+    return {
+      EVENTS,
+    };
+  },
+};
+</script>
